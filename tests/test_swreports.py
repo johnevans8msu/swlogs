e5b122129304c46a2ec8449b2f37d3f559e8828c
John Evans <john.evans8@montana.edu>
2024-11-14 09:08:37 -0700

    Refacto test suite
    For swreports, setup a test database every time.

--- tests/test_swreports.py
+++ tests/test_swreports.py
@@ -1,14 +1,38 @@
+# standard library imports
 import importlib.resources as ir
 import io
+import pathlib
+import sqlite3
+import tempfile
 import unittest
 from unittest.mock import patch
 
 # 3rd party library imports
+import pandas as pd
 
+# local imports
 from swlogs.swreports import SWReport
 
 
 class TestSuite(unittest.TestCase):
+
+    def setUp(self):
+
+        self.tdir = tempfile.TemporaryDirectory()
+
+        self.dbfile = pathlib.Path(self.tdir.name) / 'test.db'
+        conn = sqlite3.connect(self.dbfile)
+
+        path = ir.files('tests.data.swreport').joinpath('bots.csv')
+        df = pd.read_csv(path)
+        df.to_sql('bots', conn, index=False)
+
+        path = ir.files('tests.data.swreport').joinpath('overall.csv')
+        df = pd.read_csv(path)
+        df.to_sql('overall', conn, index=False)
+
+    def tearDown(self):
+        self.tdir.cleanup()
 
     def test_bots_smoke(self):
         """
@@ -17,11 +41,14 @@ class TestSuite(unittest.TestCase):
         Expected result:  report is verified
         """
 
+        newconn = sqlite3.connect(self.dbfile)
+
         with SWReport() as o:
             with patch(
                 'swlogs.swreports.sys.stdout', new=io.StringIO()
             ) as fake_stdout:
-                o.run()
+                with patch.object(o, 'conn', new=newconn):
+                    o.run()
 
                 actual = fake_stdout.getvalue()
 
