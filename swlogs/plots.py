5e2ae601e1bc9732e27fc36a71c9de6f332fbf55
John Evans <john.evans8@montana.edu>
2024-11-16 12:36:49 -0700

    Add bots plot

--- swlogs/plots.py
+++ swlogs/plots.py
@@ -4,6 +4,7 @@ import sqlite3
 import sys  # noqa : F401
 
 # 3rd party library imports
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -14,7 +15,50 @@ sns.set()
 from .common import CommonObj
 
 
+class PlotBots(CommonObj):
+    """
+    Plot hit history of top n bots
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.n = 5
+
+    def run(self):
+
+        # Get the top n bots for yesterday.
+        yesterday = date.today() - timedelta(days=1)
+        sql = f"""
+            select ua from bots
+            where date = "{yesterday.isoformat()}"
+                and ua <> "dspace-internal"
+            order by hits desc
+            limit {self.n}
+            """
+        df = pd.read_sql(sql, self.conn)
+        ua = df['ua']
+
+        # select history for those bots
+        sql = f"""
+            select date, ua, hits
+            from bots
+            where ua in ('{'\', \''.join([x for x in ua.values])}')
+            order by date asc
+        """
+        df = pd.read_sql(sql, self.conn)
+        df['date'] = pd.to_datetime(df['date'])
+
+        fig, ax = plt.subplots()
+        sns.lineplot(data=df, x='date', y='hits', hue='ua', ax=ax)
+        ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator())
+        fig.autofmt_xdate()
+        plt.show()
+
 class PlotOverall(CommonObj):
+    """
+    Plot historical hits and byte throughput
+    """
 
     def __init__(self):
         super().__init__()
